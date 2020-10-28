f417c692d4f90cdf845a664cbb92f1645bad52f2
jyntn23 <jayantonroblico23@gmail.com>
2020-10-29 02:33:20 +0800

    initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('products/', views.products),
+    path('customer/', views.customer),
+]
