1e1a40d04464bbcc358d5e0bb6d7ed4a1edad6c6
jyntn23 <jayantonroblico23@gmail.com>
2020-10-29 22:55:53 +0800

    routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customer/', views.customer),
+    path('', views.home, name="home"),
+    path('products/', views.products, name="products"),
+    path('customer/<str:pk_test>/', views.customer, name="customer"),
 ]
